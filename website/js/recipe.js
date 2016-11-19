ae466ee7135592f15a718fb46bb6b7379e791620
mazzawi93 <anthonymazzawi@gmail.com>
2016-11-19 23:11:38 +0000

    Restaurants are shown on map with markers

--- website/js/recipe.js
+++ website/js/recipe.js
@@ -2,6 +2,8 @@
 
 var geocoder;
 var map;
+var placesService;
+var infoWindow;
 
 (function($) {
     "use strict"; // Start of use strict
@@ -50,6 +52,8 @@ function initMap() {
     /* Initialize Google Services */
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
     geocoder = new google.maps.Geocoder();
+    placesService = new google.maps.places.PlacesService(map);
+    infoWindow = new google.maps.InfoWindow();
 }
 
 
@@ -59,16 +63,45 @@ function findRestaurants() {
     var location = document.getElementById("location-entry").value;
     var recipe = document.getElementById("recipe-entry").value;
 
-    /* Convert location into coordinates */
-    geocoder.geocode( {'address': location}, function(result, status) {
+    /* Convert location into coordinates and retrieve restaurants */
+    geocoder.geocode( {'address': location}, function(results, status) {
         if (status == 'OK') {
-            map.setCenter(result[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: result[0].geometry.location
-            });
+            console.log(results);
+            placesService.nearbySearch({
+                location: results[0].geometry.location,
+                radius: 2000,
+                type: ['restaurant']
+            }, callback);
+    
+            map.setCenter(results[0].geometry.location);
+            map.setZoom(14);
         } else {
             alert('Geocode was not successful for the following reason: ' + status);
         }
     });
 }
+
+
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+/* Place a marker on the map with restaurant name */
+function createMarker(place) {
+
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.setContent(place.name);
+        infoWindow.open(map,this);
+    });
+}
+
+
