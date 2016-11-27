9b99713296a49dd045002cd98c27ae3e23b2f40b
Han <entheossss1994@yahoo.com>
2016-11-27 23:27:24 +0000

    distinct recipes

--- bbcgoodfood/bbcgoodfood.py
+++ bbcgoodfood/bbcgoodfood.py
@@ -9,16 +9,12 @@ from urllib import request
 import collections
 import json
 from bs4 import BeautifulSoup
-import sys
 from scrapeing import ingredient_Extract
-from parser import *
-
-
+import re
+from fuzzywuzzy import process
 
 
 class bbcGoodFood():
-
-
     def __init__(self):
         self.colUrlList = []
         self.dishUrlList = []
@@ -36,6 +32,7 @@ class bbcGoodFood():
         req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         html = urllib.request.urlopen(req).read()
         soup = BeautifulSoup(html, 'lxml')
+
         # find div section containing url link
         article = soup.find('div', {
             'class': 'row pad-left pad-right content-recipe-categories'}).findAll('article')
@@ -48,7 +45,7 @@ class bbcGoodFood():
 
 
     def dishUrlGen(self):
-        '''asd
+        '''
         Parse and store dish url from each collection stoted in colUrlList
         '''
         print('collect dish url ' + '\n')
@@ -60,7 +57,6 @@ class bbcGoodFood():
             # loop page of each collection
             for page in page_id:
                 dishList = []
-
 
                 # create specific url
                 pagePostFix = '?page=' + str(page) + '#c'
@@ -91,31 +87,25 @@ class bbcGoodFood():
             print('store dishes from ' + str(url))
         return self.dishUrlList
 
-
     def storeDetail(self):
         '''
         Parse each dish url, store details in json format
         '''
-        print('start file writing'+'\n')
+        print('start file writing' + '\n')
         base_url = 'http://www.bbcgoodfood.com'
-        num = 0
-        f = open('ingred.json','r')
+        f = open('ingred.json', 'r')
         r = f.read()
         ingred_list = json.loads(r)
-        #recipes_list = []
+        f.close()
+        num = 0
         for dishUrl in self.dishUrlList:
 
-            # dish html parse
             req = urllib.request.Request(base_url + dishUrl, headers={'User-Agent': 'Mozilla/5.0'})
-            try:
-                html = urllib.request.urlopen(req).read()
-                soup = BeautifulSoup(html, 'lxml')
-                body = soup.body
 
-            except request.exceptions.RequestsException as e:
-                print(e)
-                sys.exit(1)
 
+            html = urllib.request.urlopen(req).read()
+            soup = BeautifulSoup(html, 'lxml')
+            body = soup.body
 
             article = soup.find('ul', {
                 'class': 'ingredients-list__group'})
@@ -127,6 +117,7 @@ class bbcGoodFood():
             ingredients = []
 
             for li in article.findAll('li'):
+                result = {'name': None, 'qty': None, 'unit': None}
                 totaltext = li.text
                 if li.find('p'):
                     totaltext = totaltext.replace((li.find('p').text), '')
@@ -135,32 +126,59 @@ class bbcGoodFood():
                     totaltext = totaltext.replace((li.find('h2').text), '')
 
                 if totaltext != '':
-                    ingredients.append(parse(totaltext))
+                    line = totaltext
+                    line = re.sub(u"½", u".5", line)
+                    line = re.sub(u"¼", u".25", line)
+
+                    numbers = re.search("^[0-9]+", line)
+                    unitofmeasure = re.search("ml|g|tbsp|tsp|slices|liters|cm|kg", line)
+                    decimal = re.search("^\d*\.\d+", line)
+                    needmultiply = re.search(u"^([0-9]+) x ([0-9]+)", line)
+
+                    # ingredient parse to unit, measure, name
+                    ingredient = ''
+                    fuzzy_match = process.extract(line, ingred_list)
+                    best_match_val = fuzzy_match[0][1]
+                    candidates = [x for x in fuzzy_match if x[1] == best_match_val]
+                    ingredient = max(candidates, key=len)[0]
+                    result = {'name': ingredient, 'qty': None, 'unit': None}
+
+                    if decimal:
+                        result['qty'] = float(decimal.group())
+                    else:
+                        if numbers:
+                            if needmultiply:
+                                result['qty'] = float(needmultiply.group(1)) * float(needmultiply.group(2))
+                            else:
+                                result['qty'] = float(numbers.group())
+                    if unitofmeasure:
+                        result['unit'] = unitofmeasure.group()
+
+                    ingredients.append(result)
+
+            acc_distinct_names = set()
+
+            for el in ingredients:
+                if el['name'] == '':
+                    ingredients.remove(el)
+                else:
+                    acc_distinct_names.add(el['name'])
+
+            ingredients_detail = str(soup.find("div", {"class": "ingredients-list"}))
+            method = str(soup.find("div", {"class": "method"}))
 
 
-
-            origin_ingred = ingredients
-            new_list = ingredients
-
-            for i in range(len(new_list)):
-                for reci in ingred_list:
-                    if reci.lower() in ingredients[i]['name']:
-                        ingredients[i]['name'] = str(reci)
-
-            # method, html
-            method = soup.find('div', {
-                'class': 'method'
-            })
-
-            # dict to store infos
+            # recipes information
             dic = collections.OrderedDict()
-            dic['recipe'] = recipeName
-            dic['ingredients'] = ingredients
-            dic['ingred_detail'] = origin_ingred
-            dic['method'] = str(method)
+            dic['name'] = recipeName
+            dic['ingredients'] = list(acc_distinct_names)
+            dic['url'] = base_url + dishUrl
+            dic['quantities'] = ingredients
+
+            dic['list_ingredients'] = ingredients_detail
+            dic['method'] = method
 
             self.recipes_list.append(dic)
-
 
             # write in json file
 
@@ -171,20 +189,30 @@ class bbcGoodFood():
             print(str(num) + ':' + 'store dish data, ' + recipeName)
             num += 1
 
-
         self.recipes_dict['Recipes'] = self.recipes_list
 
-        f = open('goodfood.json', 'w')
-        f.write(json.dumps(self.recipes_dict))
+
+
+        return self.recipes_dict
+
+    def writeToFile(self, reci, fileNameStore):
+
+        new_list = []
+        urllist = []
+        for item in reci:
+            if item['name'] not in urllist:
+                new_list.append(item)
+                urllist.append(item['name'])
+
+        print('number of recipes to store'+ ':' + len(new_list))
+        f = open(fileNameStore, 'w')
+        jd = json.dumps(new_list)
+        f.write(jd)
         f.close()
-
-        return self.recipes_list
-
 
 
 if __name__ == '__main__':
-
-    # Walk through pipeline, get a json file
+    # pipeline, get a json file
 
 
     ob = bbcGoodFood()
@@ -193,15 +221,7 @@ if __name__ == '__main__':
     print('ingred_list scrapy')
     ingredient_Extract()
 
-    ob.storeDetail()
+    recipe_list = ob.storeDetail()
+    ob.writeToFile(reci=recipe_list, fileNameStore='bbcgoodfood.json')
+
     print('--------Finish----------')
-
-
-
-
-
-
-
-
-
-
