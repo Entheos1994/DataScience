66ac1519742d2f305e6e2cb74c205c51380653ec
simimasen_lo <simimasen_lo@foxmail.com>
2016-12-15 11:42:58 +0000

    modify

--- bbc/recommend/ranking_ingredients.py
+++ bbc/recommend/ranking_ingredients.py
@@ -2,7 +2,7 @@ import json
 
 #ranking ingredient using only bbchealthy dataset
 def ranked_result_of_ingredient():
-    with open('/Users/Blair/Documents/workspace/pycharm/DataScience/datasets/bbcHealthy.json', 'r') as stream:
+    with open('/datasets/bbcHealthy.json', 'r') as stream:
         healthy_food = json.load(stream)
     # print(healthy_food)
     list = []
@@ -17,13 +17,13 @@ def ranked_result_of_ingredient():
 #ranked_result_of_ingredient()
 
 def write_ranking_result():
-    json.dump(ranked_result_of_ingredient(), open('/Users/Blair/Documents/workspace/pycharm/DataScience/datasets/ingredients_ranking_healthybbc.json','w'))
+    json.dump(ranked_result_of_ingredient(), open('/datasets/ingredients_ranking_healthybbc.json','w'))
 
 #write_ranking_result()
 
 #ranking ingredient only use goodfood
 def ranked_result_of_ingredient_goodfood():
-    with open('/Users/Blair/Documents/workspace/pycharm/DataScience/datasets/distinct/bbcgoodfoodnew.json', 'r') as stream:
+    with open('/datasets/distinct/bbcgoodfoodnew.json', 'r') as stream:
         good_food = json.load(stream)
     # print(healthy_food)
     list = []
@@ -38,7 +38,7 @@ def ranked_result_of_ingredient_goodfood():
 #ranked_result_of_ingredient_goodfood()
 
 def write_ranking_result_goodfood():
-    json.dump(ranked_result_of_ingredient_goodfood(), open('/Users/Blair/Documents/workspace/pycharm/DataScience/datasets/ingredients_ranking_bbcgoodfood.json','w'))
+    json.dump(ranked_result_of_ingredient_goodfood(), open('/datasets/ingredients_ranking_bbcgoodfood.json','w'))
 
 #write_ranking_result_goodfood()
 
@@ -46,20 +46,23 @@ def write_ranking_result_goodfood():
 #ranking ingredient using both goodfood and healthy food
 
 def ranked_result_of_ingredient_overall():
-    with open('datasets/distinct/bbcgoodfoodnew.json', 'r') as stream:
+    with open('/datasets/distinct/bbcgoodfoodnew.json', 'r') as stream:
         good_food = json.load(stream)
     # print(healthy_food)
     list = []
     for item in good_food:
         list.extend(item['ingredients'])
-    print(list)
-    with open('datasets/distinct/bbcHealthydistinct.json', 'r') as stream1:
+
+    # print(list)
+
+    with open('/datasets/distinct/bbcHealthydistinct.json', 'r') as stream1:
         healthy_food = json.load(stream1)
         # print(healthy_food)
 
     for item in healthy_food:
         list.extend(item['ingredients'])
-    print(list)
+    # print(list)
+
     d = {}
     for item in list:
         d[item] = list.count(item)
@@ -69,7 +72,7 @@ def ranked_result_of_ingredient_overall():
 
 
 def write_ranking_result_overall():
-    json.dump(ranked_result_of_ingredient_goodfood(), open('datasets/ingredients_ranking_overall.json','w'))
+    json.dump(ranked_result_of_ingredient_overall(), open('/datasets/ingredients_ranking_overall.json','w'))
 
 
 #rank both bbcHealthy dataset and bbcgoodfood dataset
